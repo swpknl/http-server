5965d6d93485550989f0a83d03021ca42171b963
Swapnil Ramteke <swapnilcramteke@gmail.com>
2024-04-06 13:48:44 +0530

    pass 2nd stage

--- src/Server.cs
+++ src/Server.cs
@@ -1,5 +1,6 @@
 using System.Net;
 using System.Net.Sockets;
+using System.Text;
 
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 Console.WriteLine("Logs from your program will appear here!");
@@ -7,4 +8,8 @@ Console.WriteLine("Logs from your program will appear here!");
 // Uncomment this block to pass the first stage
 TcpListener server = new TcpListener(IPAddress.Any, 4221);
 server.Start();
-server.AcceptSocket(); // wait for client
+var socket = server.AcceptSocket(); // wait for client
+var buffer = new ArraySegment<byte>();
+var data = await socket.ReceiveAsync(buffer, SocketFlags.None);
+var result = "HTTP/1.1 200 OK\\r\\n\\r\\n";
+await socket.SendAsync(new ArraySegment<byte>(Encoding.UTF8.GetBytes(result.ToCharArray())), SocketFlags.None);
