04656343b5c679a10017500367a8432dbab29cba
Swapnil Ramteke <swapnilcramteke@gmail.com>
2024-04-06 14:42:00 +0530

    5th stage

--- src/Server.cs
+++ src/Server.cs
@@ -3,6 +3,7 @@ using System.Net.Sockets;
 using System.Security.Cryptography;
 using System.Text;
 using System.Text.RegularExpressions;
+using System.Linq;
 
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 Console.WriteLine("Logs from your program will appear here!");
@@ -13,13 +14,20 @@ server.Start();
 var socket = server.AcceptSocket(); // wait for client
 var buffer = new byte[1024];
 var received = socket.Receive(buffer);
-var data = Encoding.UTF8.GetString(buffer).Split(" ")[1];
+var request = Encoding.UTF8.GetString(buffer); 
+var data = request.Split(" ")[1];
 string result = string.Empty;
 if (data.StartsWith("/echo"))
 {
     Console.WriteLine("in echo");
     var echoed = data.Replace("/echo/", string.Empty);
     result = $"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {echoed.Length}\r\n\r\n{echoed}";
+}
+else if (data.StartsWith("/user"))
+{
+    var userAgent = request.Split("\r\n")[2];
+    var userAgentValue = userAgent.Split(":")[1];
+    result = $"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {userAgentValue.Length}\r\n\r\n{userAgentValue}";
 }
 else
 {
