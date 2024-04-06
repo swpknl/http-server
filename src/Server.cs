6b8923cd675cb771b72960173c3d05979a0e073b
Swapnil Ramteke <swapnilcramteke@gmail.com>
2024-04-06 14:18:37 +0530

    4th stage

--- src/Server.cs
+++ src/Server.cs
@@ -14,15 +14,9 @@ var socket = server.AcceptSocket(); // wait for client
 var buffer = new byte[1024];
 var received = socket.Receive(buffer);
 var data = Encoding.UTF8.GetString(buffer).Split(" ")[1];
-string result = string.Empty;
-if (data.Length == 1 && data == "/")
-{
-    result = "HTTP/1.1 200 OK\r\n\r\n";
-}
-else
-{
-    result = "HTTP/1.1 404 OK\r\n\r\n";
-}
+var echoed = data.Split("/")[1];
+string result = string.Empty; 
+result = $"HTTP/1.1 200 OK\r\n\r\nContent-Type: text/plain\r\n\r\nContent-Length: {echoed.Length}\r\n\r\n\r\n\r\n{echoed}";
 var bytes = Encoding.UTF8.GetBytes(result);
 socket.Send(bytes);
 Console.ReadLine();
