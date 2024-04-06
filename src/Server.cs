f564df5bc52a122bbb2de322d00f06fd86f29b49
Swapnil Ramteke <swapnilcramteke@gmail.com>
2024-04-06 14:15:03 +0530

    3rd stage

--- src/Server.cs
+++ src/Server.cs
@@ -1,6 +1,8 @@
 using System.Net;
 using System.Net.Sockets;
+using System.Security.Cryptography;
 using System.Text;
+using System.Text.RegularExpressions;
 
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 Console.WriteLine("Logs from your program will appear here!");
@@ -9,6 +11,18 @@ Console.WriteLine("Logs from your program will appear here!");
 TcpListener server = new TcpListener(IPAddress.Any, 4221);
 server.Start();
 var socket = server.AcceptSocket(); // wait for client
-var result = "HTTP/1.1 200 OK\r\n\r\n";
+var buffer = new byte[1024];
+var received = socket.Receive(buffer);
+var data = Encoding.UTF8.GetString(buffer).Split(" ")[1];
+string result = string.Empty;
+if (data.Length == 1 && data == "/")
+{
+    result = "HTTP/1.1 200 OK\r\n\r\n";
+}
+else
+{
+    result = "HTTP/1.1 404 OK\r\n\r\n";
+}
 var bytes = Encoding.UTF8.GetBytes(result);
 socket.Send(bytes);
+Console.ReadLine();
