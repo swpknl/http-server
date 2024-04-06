5599789fb4c7496b2d9d88343b6cb237f8add6d2
Swapnil Ramteke <swapnilcramteke@gmail.com>
2024-04-06 15:49:55 +0530

    8th Stage

--- src/Server.cs
+++ src/Server.cs
@@ -66,6 +66,7 @@ void AcceptClient(IAsyncResult ar, string[] args)
         else
         {
             string requestBody = request.Split("\r\n\r\n")[1].TrimEnd();
+            requestBody = requestBody + "\r\n";
             Console.WriteLine(requestBody);
             string filename = data.Split("/files/")[1];
             var file = Path.Combine(directory, filename);
