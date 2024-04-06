b12238f5cbecdff0dbd94e31ae944f673df07ac3
Swapnil Ramteke <swapnilcramteke@gmail.com>
2024-04-06 14:26:38 +0530

    4th stage

--- src/Server.cs
+++ src/Server.cs
@@ -17,6 +17,7 @@ var data = Encoding.UTF8.GetString(buffer).Split(" ")[1];
 string result = string.Empty;
 if (data.StartsWith("/echo"))
 {
+    Console.WriteLine("in echo");
     var echoed = data.Replace("/echo/", string.Empty);
     result = $"HTTP/1.1 200 OK\r\n\r\nContent-Type: text/plain\r\n\r\nContent-Length: {echoed.Length}\r\n\r\n\r\n\r\n{echoed}";
 }
